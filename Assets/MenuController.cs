62e5edb841aaa2c40a0b5a9088806e8dbacf8548
drachmat <drachmat.iskandar@gmail.com>
2019-08-27 18:45:19 +0700

    Initial commit

---
+++ Assets/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject[] marker;
+    public GameObject Menu;
+    public GameObject BackMenu;
+    public string GameObjectName;
+
+    public void ChooseObject(GameObject GO)
+    {
+        for (int i = 0; i < marker.Length; i++)
+        {
+            marker[i].SetActive(false);
+        }
+
+        GO.SetActive(true);
+        GameObjectName = GO.name;
+        Menu.SetActive(false);
+    }
+
+    public void exit()
+    {
+        Application.Quit(); 
+    }
+}
